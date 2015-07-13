16d676a2198fe0b282b4210e917896178eea76be
pupuliao <pupuliao@gmail.com>
2015-07-14 00:06:39 +0800

    JNovelDownloader v4.0

--- src/JNovelDownloader/Option/About.java
+++ src/JNovelDownloader/Option/About.java
@@ -1,12 +1,12 @@
 package JNovelDownloader.Option;
 
 public class About {
-	public static String version="V 3.9";
+	public static String version="V 4.0";
 	public static String tittle="JNovelDownloader";	
 	public static String author="pupuliao";
 	public static String emailString="pupuliao@gmail.com";
 	public static String urlString="http://www.pupuliao.info";
-	public static double versionNumber=3.9;
+	public static double versionNumber=4.0;
 	public About(){
 		
 	}
