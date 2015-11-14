7f5330edce839a6bf211353008ddb97b941cf630
npes87184 <npes87184@gmail.com>
2015-11-15 00:48:26 +0800

    ReplaceElement

---
+++ src/JNovelDownloader/Kernel/ReplaceElement.java
@@ -0,0 +1,12 @@
+package JNovelDownloader.Kernel;
+
+public class ReplaceElement {
+	public String fromString;
+	public String toString; 
+	
+	
+	public ReplaceElement(String from ,String to){
+		this.fromString = from;
+		this.toString = to;
+	}
+}
